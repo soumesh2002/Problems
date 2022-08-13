dd5ff7dad39ec9be38468916ff017b4c87effec6
soumesh2002 <soumesh6622@gmail.com>
2022-08-13 12:51:57 +0530

    13-08-2022

--- RemoveCharacters.cpp
+++ RemoveCharacters.cpp
@@ -1,1 +1,22 @@
-// removing characters in a string 
+// removing characters in a string
+#include <iostream>
+using namespace std;
+int main() {
+    string line;
+    int i;
+    cout << "enter a string: ";
+    cin >> line;
+
+    cout << "\nthe original string is: " << line << endl;
+
+    int len = line.size();
+    for (i = 0; i < len; i++) {
+        if (!((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))) {
+            // for finding any other characters rather than alphabets
+            line[i] = '\0';
+        }
+    }
+
+    cout << "\nafter character removed the string: " << line << endl;
+    return 0;
+}
